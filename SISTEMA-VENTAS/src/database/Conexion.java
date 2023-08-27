a915f62ef3e9a1563cf4e648213f78e60f841ae1
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2023-08-27 03:02:20 -0500

    Conexión a MYSQL

--- SISTEMA-VENTAS/src/database/Conexion.java
+++ SISTEMA-VENTAS/src/database/Conexion.java
@@ -5,6 +5,42 @@ package database;
  *
  * @author ayeren
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
 public class Conexion {
+    private final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private final String URL = "jdbc:mysql://localhost:3306/";
+    private final String DB = "dbsistemaventas";
+    private final String USER ="root";
+    private final String PASSWORD = "root";
+    
+    public Connection cadena;
+    
+    public Conexion(){
+        this.cadena = null;
+    }
+    
+    public Connection conectar(){
+        try {
+            Class.forName(DRIVER);
+            this.cadena = DriverManager.getConnection(URL+DB,USER,PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            System.exit(0);
+        }
+        return this.cadena;
+    }
+    
+    public void desconectar(){
+        try {
+            this.cadena.close();
+        } catch (Exception e) {
+             JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
     
 }
---
+++ SISTEMA-VENTAS/src/database/PruebaConexion.java
@@ -0,0 +1,23 @@
+
+package database;
+
+/**
+ *
+ * @author ayeren
+ */
+
+
+public class PruebaConexion {
+    public static void main(String[] args) {
+        Conexion con = new Conexion();
+        con.conectar();
+        if(con.cadena != null){
+            System.out.println("Conectado");
+        }else{
+            System.out.println("Desconectado");
+        }
+        
+    }
+
+
+}
